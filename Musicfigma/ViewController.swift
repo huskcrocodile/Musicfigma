57c5e0162b307152de7bf3ae971755922143171b
Huscro <apologize9100@gmail.com>
2022-04-05 22:39:42 +0800

    Initial Commit

---
+++ Musicfigma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musicfigma
+//
+//  Created by 曾曜澤 on 2022/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
